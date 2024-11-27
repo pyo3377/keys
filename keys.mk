ff0fc2748c6967541aa2614e8089f6ce9145fe95
Ma_キーマ <75950219+pyo3377@users.noreply.github.com>
2024-11-27 22:06:08 +0700

    Update keys.mk

--- keys.mk
+++ keys.mk
@@ -1,2 +1,2 @@
 $(warning "Your build will be signed Ma_キーマ key.")
-PRODUCT_DEFAULT_DEV_CERTIFICATE := vendor/arrow/signing/keys/releasekey
+PRODUCT_DEFAULT_DEV_CERTIFICATE := vendor/derp/signing/keys/releasekey
