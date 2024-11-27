5e51662bef5992aebc11a7c992cd6dee88cc5622
Ma_キーマ <75950219+pyo3377@users.noreply.github.com>
2024-11-27 09:23:25 +0700

    Update keys.mk

--- keys.mk
+++ keys.mk
@@ -1,1 +1,2 @@
-PRODUCT_DEFAULT_DEV_CERTIFICATE := vendor/lineage-priv/keys/releasekey
+$(warning "Your build will be signed Ma_キーマ key.")
+PRODUCT_DEFAULT_DEV_CERTIFICATE := vendor/private/keys/releasekey
